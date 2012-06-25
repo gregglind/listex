9982012e81c3a3a91e0684c849cb12bb724e8042
Gregg Lind <gregg.lind@gmail.com>
2012-06-25 12:27:46 -0500

    initial commit, including pyparsing

---
+++ README.md
@@ -0,0 +1,3 @@
+# SEQUEX - regular expressions for other sequences
+
+
---
+++ sequex.py
@@ -0,0 +1,24 @@
+
+"""
+
+runs = S(
+    S({'someattr': 'somevalue'}).any(), S({'otherattr': 'othervalue'})
+)
+
+
+# as a regex:  x*y
+
+# concepts:
+#    grouping
+#    count (one or more, zero or more, etc)
+#    refernences?
+#    greedy / non-greedy
+
+
+"""
+
+
+class S(object):
+    pass
+
+
